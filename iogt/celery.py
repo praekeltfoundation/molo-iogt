a47019b95ebd8f82befaa480de5303706a06451e
Saeed Marzban <Saeed.mb8@gmail.com>
2016-05-17 10:46:14 +0200

    smal tweak

--- iogt/celery.py
+++ iogt/celery.py
@@ -6,9 +6,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'iogt.settings.production')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iogt.settings.production')
 
 app = Celery('proj')
 
