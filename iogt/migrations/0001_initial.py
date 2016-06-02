f2fbc3c6a5dbd1248ff47eee5b22dfddb9eb5930
Saeed Marzban <Saeed.mb8@gmail.com>
2016-06-02 17:32:32 +0200

    Added data migration for creating a Universal Core Importer

---
+++ iogt/migrations/0001_initial.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0035_content_rotation_settings'),
+    ]
+
+    operations = [
+
+    ]
