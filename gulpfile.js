9a1cc3096b0b124215cf851b3644c19232dbece3
Mitso <mqalaba@gmail.com>
2016-10-07 20:19:35 +0200

    remove modules structure to styles dir

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,20 @@ var gulp        =   require('gulp'),
     sourcemaps  =   require('gulp-sourcemaps'),
     sassdoc     =   require('sassdoc'),
     livereload  =   require('gulp-livereload'),
-    scss_destination = 'iogt/static/css/dest';
+    scss_destination = 'iogt/static/css';
 
 var sass_paths = [
-        'iogt/static/css/opera-mini_single-view.scss',
-        'iogt/static/css/style-rtl.scss',
-        'iogt/static/css/style.scss',
-        'iogt/static/css/state_320/state_320.scss',
-        'iogt/static/css/versions.scss'
+        'iogt/styles/opera-mini_single-view.scss',
+        'iogt/styles/style-rtl.scss',
+        'iogt/styles/style.scss',
+        'iogt/styles/state_320/state_320.scss',
+        'iogt/styles/versions.scss'
     ];
 
 gulp.task('styles', function() {
     return gulp.src(sass_paths)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(sourcemaps.write('/maps'))
     .pipe(gulp.dest(scss_destination))
@@ -39,7 +38,7 @@ gulp.task('sassdoc', function() {
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('iogt/static/css/*.scss', ['styles']);
+    gulp.watch('iogt/styles/*.scss', ['styles']);
 });
 
 gulp.task('default', ['styles']);
