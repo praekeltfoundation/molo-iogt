7daaf7aa917f61c5e036306c5c0cb7972cdb9fe6
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-17 11:42:51 +0200

    Provide STATIC_URL as part of template context for django compressor

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -129,7 +129,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'molo.core.context_processors.locale',
                 'wagtail.contrib.settings.context_processors.settings',
-                'iogt.processors.env',
+                'iogt.processors.compress_settings',
             ],
         },
     },
