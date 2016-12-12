f16aa49d25cb3d95f7428e562ea79fb360c1b120
Saeed Marzban <Saeed.mb8@gmail.com>
2016-12-12 17:34:50 +0200

    Add molo_makemessages management command

---
+++ iogt/management/commands/molo_makemessages.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from distutils.dir_util import copy_tree, remove_tree
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        fromDirectory = "ve/lib/python2.7/site-packages/molo"
+        toDirectory = "iogt/temp_ve"
+        copy_tree(fromDirectory, toDirectory)
+        call_command(
+            'makemessages',
+            ignore=['ve/*', 'admin/*', 'wagtail*', 'models.py'])
+        remove_tree(toDirectory)
