3ef4ef172775ac40caf2ba9bd7087539699bbd8b
nathanbegbie <nathanbegbie@gmail.com>
2017-01-20 14:12:33 +0200

    Updated settings according to molo upgrade
    See diff from molo v3.15.0 to v4
    https://github.com/praekelt/molo/compare/3.15.0...4.0.0

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -171,9 +171,21 @@ BROKER_URL = environ.get('BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = environ.get(
     'CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 CELERYBEAT_SCHEDULE = {
-    'rotate_content': {
+    'rotate_promote_content': {
         'task': 'molo.core.tasks.rotate_content',
         'schedule': crontab(minute=0),
+    },
+    'demote_articles': {
+        'task': 'molo.core.tasks.demote_articles',
+        'schedule': crontab(minute="*"),
+    },
+    'promote_articles': {
+        'task': 'molo.core.tasks.promote_articles',
+        'schedule': crontab(minute="*"),
+    },
+    'publish_pages': {
+        'task': 'molo.core.tasks.publish_scheduled_pages',
+        'schedule': crontab(minute='*'),
     },
 }
 
