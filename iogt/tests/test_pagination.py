7af4b90a90ffdb4cefb62b95aebce84d77941646
Mitso <mqalaba@gmail.com>
2017-03-13 13:25:24 +0200

    update tests

--- iogt/tests/test_pagination.py
+++ iogt/tests/test_pagination.py
@@ -25,11 +25,15 @@ class RegistrationViewTest(TestCase, MoloTestCaseMixin):
         response = self.client.get('/sections/english-section/')
 
         self.assertNotContains(response, 'Prev')
-        self.assertContains(response, '<a href="?p=2" class="pagination__page">2</a>')
-        self.assertContains(response, '<a href="?p=3" class="pagination__page">3</a>')
+        self.assertContains(response,
+        '<a href="?p=2" class="pagination__page">2</a>')
+        self.assertContains(response,
+        '<a href="?p=3" class="pagination__page">3</a>')
         self.assertContains(response, '...')
-        self.assertContains(response, '<a href="?p=15" class="pagination__page">15</a>')
-        self.assertContains(response, '<a href="?p=16" class="pagination__page">16</a>')
+        self.assertContains(response,
+        '<a href="?p=15" class="pagination__page">15</a>')
+        self.assertContains(response,
+        '<a href="?p=16" class="pagination__page">16</a>')
         self.assertContains(response, 'Next')
 
     def test_second_page_pagination(self):
@@ -46,27 +50,41 @@ class RegistrationViewTest(TestCase, MoloTestCaseMixin):
     def test_third_page_pagination(self):
         response = self.client.get('/sections/english-section/?p=3')
         self.assertContains(response, 'Prev')
-        self.assertContains(response, '<a href="?p=1" class="pagination__page">1</a>')
-        self.assertContains(response, '<a href="?p=2" class="pagination__page">2</a>')
-        self.assertContains(response, '<a href="?p=4" class="pagination__page">4</a>')
-        self.assertContains(response, '<a href="?p=5" class="pagination__page">5</a>')
+        self.assertContains(response,
+        '<a href="?p=1" class="pagination__page">1</a>')
+        self.assertContains(response,
+        '<a href="?p=2" class="pagination__page">2</a>')
+        self.assertContains(response,
+        '<a href="?p=4" class="pagination__page">4</a>')
+        self.assertContains(response,
+        '<a href="?p=5" class="pagination__page">5</a>')
         self.assertContains(response, '...')
-        self.assertContains(response, '<a href="?p=15" class="pagination__page">15</a>')
-        self.assertContains(response, '<a href="?p=16" class="pagination__page">16</a>')
+        self.assertContains(response,
+        '<a href="?p=15" class="pagination__page">15</a>')
+        self.assertContains(response,
+        '<a href="?p=16" class="pagination__page">16</a>')
         self.assertContains(response, 'Next')
 
     def test_fifth_page_pagination(self):
         response = self.client.get('/sections/english-section/?p=5')
         self.assertContains(response, 'Prev')
-        self.assertContains(response, '<a href="?p=1" class="pagination__page">1</a>')
-        self.assertContains(response, '<a href="?p=2" class="pagination__page">2</a>')
-        self.assertContains(response, '<a href="?p=3" class="pagination__page">3</a>')
-        self.assertContains(response, '<a href="?p=4" class="pagination__page">4</a>')
-        self.assertContains(response, '<a href="?p=6" class="pagination__page">6</a>')
-        self.assertContains(response, '<a href="?p=7" class="pagination__page">7</a>')
+        self.assertContains(response,
+        '<a href="?p=1" class="pagination__page">1</a>')
+        self.assertContains(response,
+        '<a href="?p=2" class="pagination__page">2</a>')
+        self.assertContains(response,
+        '<a href="?p=3" class="pagination__page">3</a>')
+        self.assertContains(response,
+        '<a href="?p=4" class="pagination__page">4</a>')
+        self.assertContains(response,
+        '<a href="?p=6" class="pagination__page">6</a>')
+        self.assertContains(response,
+        '<a href="?p=7" class="pagination__page">7</a>')
         self.assertContains(response, '...')
-        self.assertContains(response, '<a href="?p=15" class="pagination__page">15</a>')
-        self.assertContains(response, '<a href="?p=16" class="pagination__page">16</a>')
+        self.assertContains(response,
+        '<a href="?p=15" class="pagination__page">15</a>')
+        self.assertContains(response,
+        '<a href="?p=16" class="pagination__page">16</a>')
         self.assertContains(response, 'Next')
 
     def test_fourteenth_page_pagination(self):
@@ -107,12 +125,12 @@ class RegistrationViewTest(TestCase, MoloTestCaseMixin):
         response = self.client.get('/sections/english-section/?p=16')
         self.assertContains(response, 'Prev')
         self.assertContains(response,
-        '<a href="?p=1" class="pagination__page">1</a>')
+            '<a href="?p=1" class="pagination__page">1</a>')
         self.assertContains(response,
-        '<a href="?p=2" class="pagination__page">2</a>')
+            '<a href="?p=2" class="pagination__page">2</a>')
         self.assertContains(response, '...')
         self.assertContains(response,
-        '<a href="?p=14" class="pagination__page">14</a>')
+            '<a href="?p=14" class="pagination__page">14</a>')
         self.assertContains(response,
-        '<a href="?p=15" class="pagination__page">15</a>')
+            '<a href="?p=15" class="pagination__page">15</a>')
         self.assertNotContains(response, 'Next')
