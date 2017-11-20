c6bd2569a75b41564c5b63753b5129a9d63603ff
nathanbegbie <nathanbegbie@gmail.com>
2017-11-20 10:57:50 +0200

    rename middleware variable to lower case

--- iogt/middleware.py
+++ iogt/middleware.py
@@ -4,9 +4,9 @@ from django.http import HttpResponsePermanentRedirect
 
 class SSLRedirectMiddleware(object):
     def process_request(self, request):
-        HTTPS_PATHS = getattr(settings, 'HTTPS_PATHS', [])
+        https_paths = getattr(settings, 'HTTPS_PATHS', [])
         response_should_be_secure = self.response_should_be_secure(
-            request, HTTPS_PATHS)
+            request, https_paths)
         request_is_secure = self.request_is_secure(request)
         if response_should_be_secure and not request_is_secure:
             return HttpResponsePermanentRedirect(
@@ -14,8 +14,8 @@ class SSLRedirectMiddleware(object):
                                       request.get_full_path())
             )
 
-    def response_should_be_secure(self, request, HTTPS_PATHS):
-        for path in HTTPS_PATHS:
+    def response_should_be_secure(self, request, https_paths):
+        for path in https_paths:
             if request.path.startswith(u'/{}'.format(path)):
                 return True
         return False
