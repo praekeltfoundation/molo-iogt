611aaf2f662ad83b9250b31ef0c357b92003c563
nathanbegbie <nathanbegbie@gmail.com>
2017-11-21 14:16:44 +0200

    Create better functions for handling URL format
    Make sure that when dealing with URL paths we are using them in a consistent format: a list of strings representing each segment of the path

--- iogt/middleware.py
+++ iogt/middleware.py
@@ -1,12 +1,49 @@
+from urlparse import urlparse
+
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect
+
+
+def clean_path(path):
+    '''
+    Converts string of URL paths to list of path elements
+    '''
+    if path == u'/':
+        return [u'/']
+    else:
+        return filter(lambda a: a != '', path.split('/'))
+
+
+def clean_paths(paths):
+    '''
+    Converts list of URL paths to list of
+    lists containing only the strings making up the path
+
+    sample input:
+    ['/admin', '/admin/', 'profile/login/']
+
+    sample output:
+    [['admin'], ['admin'], ['profile', 'login']]
+    '''
+    return [clean_path(path) for path in paths]
+
+
+def is_match(path, possible_matches):
+    '''
+    Matches segments of a URL path against a list of possible matches
+    '''
+    for possible_match in possible_matches:
+        if possible_match == path[:len(possible_match)]:
+            return True
+    return False
 
 
 class SSLRedirectMiddleware(object):
     def process_request(self, request):
         https_paths = getattr(settings, 'HTTPS_PATHS', [])
+        clean_https_paths = clean_paths(https_paths)
         response_should_be_secure = self.response_should_be_secure(
-            request, https_paths)
+            request, clean_https_paths)
         request_is_secure = self.request_is_secure(request)
         if response_should_be_secure and not request_is_secure:
             return HttpResponsePermanentRedirect(
@@ -15,10 +52,8 @@ class SSLRedirectMiddleware(object):
             )
 
     def response_should_be_secure(self, request, https_paths):
-        for path in https_paths:
-            if request.path.startswith(u'/{}'.format(path)):
-                return True
-        return False
+        request_path = clean_path(request.path)
+        return is_match(request_path, https_paths)
 
     def request_is_secure(self, request):
         if 'HTTP_X_FORWARDED_PROTO' in request.META:
--- iogt/tests/test_middleware.py
+++ iogt/tests/test_middleware.py
@@ -8,10 +8,68 @@ from django.test import (
 from molo.core.tests.base import MoloTestCaseMixin
 from molo.core.models import Main
 
-from iogt.middleware import SSLRedirectMiddleware
+from iogt.middleware import (
+    SSLRedirectMiddleware,
+    clean_path,
+    clean_paths,
+    is_match,
+)
 
 
 PERMANENT_REDIRECT_STATUS_CODE = 301
+
+
+class TestMiddlewareUtils(TestCase):
+    def test_clean_path(self):
+        self.assertEqual(clean_path(u'/'), [u'/'])
+        self.assertEqual(clean_path('/admin'), ['admin'])
+        self.assertEqual(clean_path('/admin/'), ['admin'])
+        self.assertEqual(clean_path('admin/'), ['admin'])
+        self.assertEqual(clean_path('admin'), ['admin'])
+        self.assertEqual(clean_path('admin/login'), ['admin', 'login'])
+        self.assertEqual(clean_path('/admin/login/'), ['admin', 'login'])
+        self.assertEqual(clean_path('admin/login'), ['admin', 'login'])
+        self.assertEqual(clean_path('/admin/login'), ['admin', 'login'])
+        self.assertEqual(clean_path('/admin/login/extra'),
+                         ['admin', 'login', 'extra'])
+
+    def test_clean_paths(self):
+        self.assertEqual(
+            clean_paths([]), []
+        )
+        self.assertEqual(
+            clean_paths(['/admin', '/admin/', 'admin/', 'admin']),
+            [['admin'], ['admin'], ['admin'], ['admin']]
+        )
+
+    def test_is_match(self):
+        self.assertTrue(
+            is_match(['admin'], [['admin']])
+        )
+        self.assertFalse(
+            is_match(['admin'], [['admin-django']])
+        )
+        self.assertFalse(
+            is_match(['profiles'], [['profiles', 'login']])
+        )
+        self.assertTrue(
+            is_match(['profiles', 'login'], [['profiles', 'login']])
+        )
+        self.assertFalse(
+            is_match(['profiles', 'login'], [['profiles', 'register']])
+        )
+        self.assertTrue(
+            is_match(['profiles', 'login'], [['admin'], ['profiles', 'login']])
+        )
+        self.assertFalse(
+            is_match(['profiles', 'login'], [])
+        )
+        self.assertTrue(
+            is_match(['/'], [['/']])
+        )
+        self.assertTrue(
+            is_match(['/'], [['admin'], ['/']])
+        )
 
 
 @override_settings(HTTPS_PATHS=['admin'])
