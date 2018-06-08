95d8fc12a33a06b9058e8bbfec69a0ff743ed0ce
Aphiwe Moshesh <aphiwe@praekelt.org>
2018-06-08 11:07:29 +0200

    update the repo to molo v5-22-5
    add site language relation model instance to breakig tests
    update project requirements
    add wagtails_personalsation to INSTALLED_APPS

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'wagtail.contrib.wagtailsitemaps',
     'wagtail.contrib.modeladmin',
     'wagtailsurveys',
+    'wagtail_personalisation',
 
     'mptt',
     'molo.usermetadata',
