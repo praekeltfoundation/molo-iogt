4bd2d5a8dd7e32c91325ce030a54673bc12361cf
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-06-19 15:22:39 +0200

    add localhost to allowed hosts in dev settings

--- iogt/settings/dev.py
+++ iogt/settings/dev.py
@@ -8,6 +8,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 BROKER_URL = 'amqp://rabbit:secret@rabbitmq.com:5672/molo-iogt'
 
+ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
 
 try:
     from .local import *  # noqa
