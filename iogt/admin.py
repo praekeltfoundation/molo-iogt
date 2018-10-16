6e909796e5f226b2243fbc921a87a52761d1e8f7
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-10-16 12:00:38 +0200

    flake

--- iogt/admin.py
+++ iogt/admin.py
@@ -1,11 +1,7 @@
 from wagtail.wagtailcore import hooks
 
-# Monkey Patch to remove Reaction Questions Menu in admin
-
 
 @hooks.register('construct_main_menu')
 def show_reactionquestions(request, menu_items):
         menu_items[:] = [
             item for item in menu_items if item.name != 'reactionquestions']
-
-
