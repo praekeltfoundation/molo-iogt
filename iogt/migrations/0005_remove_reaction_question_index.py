baf041828d94f4b6b19db39e621c0c0126577c4d
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-10-16 13:29:10 +0200

    add emit_post_migrate_signal to migration

--- iogt/migrations/0005_remove_reaction_question_index.py
+++ iogt/migrations/0005_remove_reaction_question_index.py
@@ -3,20 +3,24 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.core.management.sql import emit_post_migrate_signal
 
 
 def remove_reaction_question_index(apps, schema_editor):
     """ Search for the Reaction Question Index then delete it """
 
+    db_alias = schema_editor.connection.alias
+    emit_post_migrate_signal(2, False, db_alias)
+
     ReactionQuestionIndexPage = apps.get_model(
         'core', 'ReactionQuestionIndexPage')
-    db_alias = schema_editor.connection.alias
     ReactionQuestionIndexPage.objects.using(db_alias).all().delete()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('core', '0060_reaction_questions_index_page'),
         ('iogt', '0004_create_me_expert'),
     ]
 
